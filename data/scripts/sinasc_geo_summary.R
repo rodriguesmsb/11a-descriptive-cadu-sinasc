559d96968e7e5ec14cadb7ec64bfca36c655d522
hafen <rhafen@gmail.com>
2018-10-24 11:46:58 -0700

    Update SINASC geo script

--- data/scripts/sinasc_geo_summary.R
+++ data/scripts/sinasc_geo_summary.R
@@ -1,4 +1,21 @@
+library(tidyverse)
+# devtools::install_github("ki-tools/brazilgeo")
+library(brazilgeo)
 
-sinasc <- kitools::data_use("sinasc_2001-2015")
+snsc <- kitools::data_use("snsc_2001-2015")
 
+# group_vars <- vars(birth_year, birth_state_code)
 
+geo_summary <- function(group_vars) {
+  snsc %>%
+    group_by_at(group_vars) %>%
+    summarise(
+      n_birth = n()
+      # ...
+    )
+}
+
+snsc_geo_state <- geo_summary(vars(birth_year, birth_state_code))
+snsc_geo_micro <- geo_summary(vars(birth_year, birth_micro_code))
+snsc_geo_meso <- geo_summary(vars(birth_year, birth_meso_code))
+snsc_geo_muni <- geo_summary(vars(birth_year, birth_muni_code))
